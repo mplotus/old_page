ad0401b688350cedf4698e80935dbc1e4d63ab1d
mplotus <mplotus.github.io@gmail.com>
2022-10-28 15:47:18 +0700

    Update menu button

--- code/script.js
+++ code/script.js
@@ -1,5 +1,16 @@
 function page_load() {
-    document.getElementById('get-info').innerHTML = 'Viewport is: ' + 
-    window.innerWidth + ' x ' + window.innerHeight + ' client is: ' +
-    document.documentElement.clientWidth + ' x ' + document.documentElement.clientHeight;
+    var coll = document.getElementById('body-page').getElementsByClassName('ex-button');
+    //alert(coll.length);
+    /*var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        });
+    }*/
 }
