d59e524ec65930afeb8f28cf5c4f9590f099964a
mplotus <mplotus.github.io@gmail.com>
2022-11-13 18:59:55 +0700

    Update wall and logo

--- code/script.js
+++ code/script.js
@@ -1,8 +1,10 @@
-var left_navigator;
+var left_navigator, left_arrow;
 const page_load = () => {
     left_navigator = document.getElementsByClassName('left_navigator')[0];
+    left_arrow = document.getElementsByClassName('left_arrow')[0];
 }
 const left_show_click = () => {
     //console.log('clicked');
     left_navigator.classList.toggle('left_nav_show');
+    left_arrow.classList.toggle('left_arr_rotate');
 }
