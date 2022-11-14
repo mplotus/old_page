7e7046b6ad35ba5baee3673cfe49fff9feee7eb6
mplotus <mplotus.github.io@gmail.com>
2022-11-14 14:56:19 +0700

    Update about panel

--- code/script.js
+++ code/script.js
@@ -1,10 +1,14 @@
-var left_navigator, left_arrow;
+var left_navigator, left_arrow, about_content;
 const page_load = () => {
     left_navigator = document.getElementsByClassName('left_navigator')[0];
     left_arrow = document.getElementsByClassName('left_arrow')[0];
+    about_content = document.getElementsByClassName('about_content')[0];
 }
 const left_show_click = () => {
-    //console.log('clicked');
     left_navigator.classList.toggle('left_nav_show');
     left_arrow.classList.toggle('left_arr_rotate');
+}
+const about_label_click = () => {
+    //console.log('clicked');
+    about_content.classList.toggle('about_cnt_show');
 }
