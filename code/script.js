b0f213f992e2343803a7218ae809d7a2faa0e326
mplotus <mplotus.github.io@gmail.com>
2022-11-15 10:36:49 +0700

    Update address bar and sticky address bar

--- code/script.js
+++ code/script.js
@@ -3,13 +3,19 @@ const page_load = () => {
     left_navigator = document.getElementsByClassName('left_navigator')[0];
     left_arrow = document.getElementsByClassName('left_arrow')[0];
     about_content = document.getElementsByClassName('about_content')[0];
-    document.getElementById('about_info').innerText = window.innerWidth + ' x ' + window.innerHeight;
+}
+const page_scroll = () => {
+    var addBarTop = 0.25 * window.innerHeight + 60;
+    if(document.body.scrollTop > addBarTop) 
+        document.getElementById('addstick_bar').style.display = 'block';
+    else
+        document.getElementById('addstick_bar').style.display = 'none';
 }
 const left_show_click = () => {
     left_navigator.classList.toggle('left_nav_show');
     left_arrow.classList.toggle('left_arr_rotate');
 }
 const about_label_click = () => {
-    //console.log('clicked');
     about_content.classList.toggle('about_cnt_show');
+    console.log(document.body.scrollTop);
 }
