b41ec322c7d2818125073a808a1465949aedb06a
mplotus <mplotus.github.io@gmail.com>
2022-11-18 16:03:02 +0700

    Update left menu

--- code/script.js
+++ code/script.js
@@ -5,30 +5,8 @@ const page_load = () => {
     left_arrow = document.getElementsByClassName('left_arrow')[0];
     about_content = document.getElementsByClassName('about_content')[0];
     address_path = document.getElementById('address_path');
-    //console.log(document.getElementById('body_page').childNodes.length); = 3
-}
-const page_scroll = () => {
-    var addBarTop = 0.25 * window.innerHeight + 60;
-    if(document.body.scrollTop > addBarTop) 
-        document.getElementById('addstick_bar').style.display = 'block';
-    else
-        document.getElementById('addstick_bar').style.display = 'none';
-    change_path(selectedPath);
-}
-const change_path = _str => {
-    if(_str!='' && _str !=NaN) {
-        document.getElementById('addstick_path').innerText = _str;
-        document.getElementById('address_path').innerText = _str;
-    }
-}
-const left_show_click = () => {
-    // Show left panel and make an effect
-    left_navigator.classList.toggle('left_nav_show');
-    left_arrow.classList.toggle('left_arr_rotate');
-    // Prepare left contain
-    let nameWidth = document.getElementById('left_name').offsetHeight;
+    // Load data from xml
     let leftContain = document.getElementById('left_contain');
-    leftContain.style.height = 'calc(100% - ' + (nameWidth + 28).toString() + 'px)';
     // Clear left contain before add data
     while(leftContain.childNodes.length > 0) {
         leftContain.removeChild(leftContain.childNodes[0]);
@@ -62,6 +40,28 @@ const left_show_click = () => {
         })
     });
 }
+const page_scroll = () => {
+    var addBarTop = 0.25 * window.innerHeight + 60;
+    if(document.body.scrollTop > addBarTop) 
+        document.getElementById('addstick_bar').style.display = 'block';
+    else
+        document.getElementById('addstick_bar').style.display = 'none';
+    change_path(selectedPath);
+}
+const change_path = _str => {
+    if(_str!='' && _str !=NaN) {
+        document.getElementById('addstick_path').innerText = _str;
+        document.getElementById('address_path').innerText = _str;
+    }
+}
+const left_show_click = () => {
+    // Show left panel and make an effect
+    left_navigator.classList.toggle('left_nav_show');
+    left_arrow.classList.toggle('left_arr_rotate');
+    // Prepare left contain
+    let nameHeight = document.getElementById('left_name').offsetHeight;
+    document.getElementById('left_cover').style.height = 'calc(100vh - ' + (nameHeight+ 28).toString() + 'px)';
+}
 const btt_dir_click = e => {
     let leftContain = document.getElementById('left_contain');
     let prTitle = e.currentTarget.title;
@@ -79,6 +79,8 @@ const btt_dir_click = e => {
             }
         }
     }
+    selectedPath = prTitle;
+    change_path(selectedPath);
 }
 const extrPath = _path => {
     if(_path.substring(0,1)!='/') return NaN;
