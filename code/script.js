e60b9e87fc0c465cac67bca9d2a9d3aced25f310
mplotus <mplotus.github.io@gmail.com>
2022-11-18 15:08:46 +0700

    Temp menu

--- code/script.js
+++ code/script.js
@@ -25,12 +25,82 @@ const left_show_click = () => {
     // Show left panel and make an effect
     left_navigator.classList.toggle('left_nav_show');
     left_arrow.classList.toggle('left_arr_rotate');
-    // Load data from xml to left panel
+    // Prepare left contain
     let nameWidth = document.getElementById('left_name').offsetHeight;
     let leftContain = document.getElementById('left_contain');
-    leftContain.style.height = 'calc(100% - ' + (nameWidth + 8).toString() + 'px)';
-    
-
+    leftContain.style.height = 'calc(100% - ' + (nameWidth + 28).toString() + 'px)';
+    // Clear left contain before add data
+    while(leftContain.childNodes.length > 0) {
+        leftContain.removeChild(leftContain.childNodes[0]);
+    }
+    // Add data from xml file
+    fetch('./code/data.xml').then(res => {
+        res.text().then(xml => {
+            let parser = new DOMParser();
+            let dirs = parser.parseFromString(xml, 'application/xml').querySelectorAll('folder');
+            for(i=0; i<dirs.length;i++) {
+                let curElement = dirs[i];
+                let fullPath = dirs[i].id;
+                let leftPadding = 0;
+                while(curElement.parentElement.id != '/') {
+                    fullPath = curElement.parentElement.id + '/' + fullPath;
+                    leftPadding += 20;
+                    curElement = curElement.parentElement;
+                }
+                fullPath = '/' + fullPath;
+                let btt_dir = document.createElement('div');
+                btt_dir.innerText = dirs[i].id;
+                btt_dir.title = fullPath;
+                btt_dir.classList.add('btt_dirs');
+                btt_dir.style.paddingLeft = (10 + leftPadding).toString() + 'px';
+                btt_dir.style.width = 'calc(98% - ' + (10 + leftPadding).toString() + 'px)';
+                if(dirs[i].parentElement.id == '/') btt_dir.style.display = 'block';
+                else btt_dir.style.display = 'none';
+                btt_dir.addEventListener('click', btt_dir_click);
+                leftContain.appendChild(btt_dir);
+            }
+        })
+    });
+}
+const btt_dir_click = e => {
+    let leftContain = document.getElementById('left_contain');
+    let prTitle = e.currentTarget.title;
+    let i;
+    for(i=0;i<leftContain.childNodes.length;i++) {
+        let iTitle = leftContain.childNodes[i].title;
+        if(leftContain.childNodes[i].style.display == 'block') {
+            if((iTitle.length > prTitle.length) && (iTitle.substring(0, prTitle.length) == prTitle)) {
+                leftContain.childNodes[i].style.display = 'none';
+            }
+        }
+        else {
+            if((iTitle.length > prTitle.length) && (iTitle.substring(0, prTitle.length) == prTitle) && (cntSplash(iTitle) == cntSplash(prTitle) + 1)) {
+                leftContain.childNodes[i].style.display = 'block';
+            }
+        }
+    }
+}
+const extrPath = _path => {
+    if(_path.substring(0,1)!='/') return NaN;
+    else {
+        let arrPath = new Array();
+        let iChr = 1;
+        for(i=1;i<_path.length;i++) {
+            if(_path.substring(i,i+1)=='/') {
+                arrPath.push(_path.substring(iChr, i));
+                iChr = i+1;
+            }
+        }
+        arrPath.push(_path.substring(iChr, _path.length));
+        return arrPath;
+    }
+}
+const cntSplash = _path => {
+    let nSpl = 0;
+    for(i=0;i<_path.length;i++) {
+        if(_path.substring(i,i+1)=='/') nSpl++;
+    }
+    return nSpl;
 }
 const about_label_click = () => {
     about_content.classList.toggle('about_cnt_show');
